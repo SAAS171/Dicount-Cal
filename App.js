6e95ce0b09ccea885c525d69f63a225fc1fdda38
SAAS171 <53257963+SAAS171@users.noreply.github.com>
2020-11-27 23:01:31 +0500

    pre task 1

--- App.js
+++ App.js
@@ -1,21 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React,{useState} from 'react';
+import { Text, View, StyleSheet,TextInput,Button } from 'react-native';
+import Constants from 'expo-constants';
+
 
 export default function App() {
+  const [price,setPrice]=useState("");
+  const [discount,setDiscount]=useState('');
+  const [save,setSave]=useState('');
+  const [fprice,setFprice]=useState('');
+
+  const Usave=(Price,Discount)=>{
+
+    
+
+
+
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Enter Price</Text>
+      <TextInput
+      style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
+      onChangeText={text => setPrice(text)}
+      value={price}
+    />
+    <TextInput
+      style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
+      onChangeText={text => setDiscount(text)}
+      value={discount}
+    />
+
+
+       
+     
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  
+})
