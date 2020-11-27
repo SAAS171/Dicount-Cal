2fe3e68e9cdf3681ec27e7de33a2361b9a5223e2
SAAS171 <53257963+SAAS171@users.noreply.github.com>
2020-11-27 23:44:50 +0500

    task 2

--- App.js
+++ App.js
@@ -1,63 +1,63 @@
-import React,{useState} from 'react';
-import { Text, View, StyleSheet,TextInput,Button } from 'react-native';
+import React, { useState } from 'react';
+import { Text, View, StyleSheet, TextInput, Button } from 'react-native';
 import Constants from 'expo-constants';
 
 
 export default function App() {
-  const [price,setPrice]=useState("");
-  const [discount,setDiscount]=useState('');
-  const [save,setSave]=useState('');
-  const [fprice,setFprice]=useState('');
-  function  discountCa(price,discount){
+  const [price, setPrice] = useState("");
+  const [discount, setDiscount] = useState('');
+  const [save, setSave] = useState('');
+  const [fprice, setFprice] = useState('');
+  function discountCa(price, discount) {
 
-      var dis=price*(discount/100);
-      return dis;
-
-  }
-  const fpri=()=>{
-
-    setFprice(price-discountCa(price,discount));
-
-
-
+    var dis = price * (discount / 100);
+    return dis;
 
   }
-  const saving=()=>{
+  const fpri = () => {
+
+    setFprice(() => price - discountCa(price, discount));
+
+
+
+
+  }
+  const saving = () => {
     setSave(discountCa);
 
 
 
 
   }
-  
+
 
   return (
     <View style={styles.container}>
       <Text>Enter Price</Text>
       <TextInput
-      style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
-      onChangeText={price => setPrice(price)}
-      value={price}
-    />
-    <Text>Enter Discount</Text>
-    <TextInput
-      style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
-      onChangeText={discount => setDiscount(discount)}
-      value={discount}
-    />
+        style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
+        onChangeText={price => setPrice(price)}
+        value={price}
+      />
+      <Text>Enter Discount</Text>
+      <TextInput
+        style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
+        onChangeText={discount => setDiscount(discount)}
+        value={discount}
+      />
 
-    <Button  title="Cal"
-    onPress={()=>{discountCa(price,discount)}}/>
-<Text>finalprice: {fprice} </Text>
-<Text>USave: {save}</Text>
+      <Button title="Cal"
+        onPress={() => { discountCa(price, discount) }} />
+      <Text>finalprice: {fprice} </Text>
+      <Text>USave: {save}</Text>
 
 
-       
-     
+
+
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  
+
 })
